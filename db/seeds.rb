fc5370388ce5b0903ae6faecde2693e27e186495
yungcheng <yungcheng111023@gmail.com>
2018-09-13 09:35:07 +0800

    add seed user for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+User.create(name: "test", email: "test@test.com", password: "12345678", role: "admin")
+puts "Default admin created!"
